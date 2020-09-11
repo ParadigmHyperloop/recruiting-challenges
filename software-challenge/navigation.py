0a8441252dde9b849c5634a1d1fc1cfe0dbc05cb
Daniel Burke <danielseanburke@gmail.com>
2020-09-10 21:44:17 -0230

    finish steering class, and outline navigation class to be completed by recruits

--- software-challenge/navigation.py
+++ software-challenge/navigation.py
@@ -1,5 +1,5 @@
 """
-You will be implementing a hypothetical navigation sensor class,
+You will be implementing a hypothetical navigation class,
 which keeps track of the boring machine's current position, its desired position,
 and makes any adjustments necessary to keep the machine on track.You will make the
 assumption that two other interfaces are written to make your life easier, the GPS
@@ -9,15 +9,63 @@ right (using left, right, and distance to as opposed to actual coordinates
 for simplicity)
 """
 
-"""Class to control position based on GPS input
+""" VISUALIZATION OF COORDINATE PLANE FOR STEERING
+
+x-Plane
+    behind target --------------- target
+
+y-Plane
+    left of target ---------------target------------------ right of target
+
+z-Plane
+    above target
+    |
+    |
+    |
+    |
+    |
+    target
+    |
+    |
+    |
+    |
+    |
+    below target
 """
+
+""" Class to control position based on GPS input """
 class Navigation:
     def __init__(self, GPS, steering):
         self.GPS = GPS
         self.steering = steering
-        self.position = None
-    def update_position(self, GPS):
+
+        # These will be tuples of the form (x, y, z)
+        self.current_position = None
+        self.desired_position = None
+    
+    """ Sets the desired position that the borer will attempt to move to
+    
+    Note: assume the user will pass in the desired_position parameter when using
+          the interface
+    """
+    def set_desired_position(self, desired_position):
         pass
+
+    """ Updates the current position """
+    def update_current_position(self):
+        pass
+
+    """ Navigate to the desired position from the current position
+    
+    Based on the current position tuple, compared to the desired position tuple,
+    make decisions on steering, and ensure that the actuations are successful
+
+    Returns: True if actuation requests were successful, False if not
+    Note: It may be good to notify the user if something unexpected happens!
+    """
+    def navigate(self):
+        pass
+
 
 # Code below is provided for you, DO NOT NEED TO IMPLEMENT
 
@@ -25,9 +73,9 @@ class Navigation:
 class GPS:
     def __init__(self, GPSSensor):
         self.GPS = GPSSensor
-        self.leftOf = GPSSensor.pos.left
-        self.rightOf = GPSSensor.pos.right
-        self.tunnelPos = GPSSensor.pos.track
+        self.x = GPSSensor.pos.x
+        self.y = GPSSensor.pos.y
+        self.z = GPSSensor.pos.z
 
     """ Polls the GPS for the current position
 
@@ -35,17 +83,14 @@ class GPS:
     position
     """
     def pollSensor(self):
-        self.leftOf, self.rightOf, self.tunnelPos = self.GPS.updatePosition()
+        self.x, self.y, self.z = self.GPS.updatePosition()
     
     """ Returns the positions stored in the leftOf, rightOf, and tunnelPos fields as a tuple """
     def getPos(self):
-        return self.leftOf, self.rightOf, self.tunnelPos
+        return self.x, self.y, self.z
 
-""" Interfaces with the actuator which steers the tunnel borer """
+""" Interfaces with the actuator which steers the tunnel borer, and controls its speed """
 class Steering:
-
-    """
-    """
     def __init__(self, actuation_component):
         self.act = actuation_component
 
@@ -55,3 +100,38 @@ class Steering:
     """
     def move_left(self, left_distance):
         return self.act.steer_left(left_distance)
+        
+    """ Steers the borer to the right
+
+    Returns: True if the actuation is successful, False if it is not
+    """
+    def move_right(self, right_distance):
+        return self.act.steer_right(right_distance)
+
+    """ Steers the borer down
+
+    Returns: True if the actuation is successful, False if it is not
+    """
+    def move_down(self, down_distance):
+        return self.act.steer_down(down_distance)
+
+    """ Steers the borer to the right
+
+    Returns: True if the actuation is successful, False if it is not
+    """
+    def move_up(self, up_distance):
+        return self.act.steer_up(up_distance)
+
+    """ Moves the borer forward
+
+    Returns: True if the motor sucessfully moves the borer, False if it is not
+    """
+    def move_forward(self):
+        return self.act.forward()
+
+    """ Stops the borer from moving
+
+    Returns: True if the motor stops the borer, False if it does not
+    """
+    def stop(self):
+        return self.act.stop()
