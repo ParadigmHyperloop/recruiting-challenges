bf6791255e732f3673b197b5df3b3ef9efe67172
colton-smith <cnsmith@mun.ca>
2020-09-12 19:13:45 -0230

    Update docstrings and change some wording

--- fall-2020/navigation.py
+++ fall-2020/navigation.py
@@ -1,6 +1,7 @@
-"""
+""" navigation.py
+
 You will be implementing a hypothetical navigation class,
-which keeps track of the boring machine's current position, its desired position,
+which keeps track of the TBM's current position, its desired position,
 and makes any adjustments necessary to keep the machine on track.You will make the
 assumption that two other interfaces are written to make your life easier, the GPS
 class will interface with our onboard GPS sensor to get thecurrent position, and
@@ -31,10 +32,27 @@ z-Plane
     |
     |
     below target
+    
+Misc:
+    TBM - Tunnel Boring Machine
 """
 
-""" Class to control position based on GPS input """
+# Implement this class - this code will not actually be run, it is more of a thought exercise!
 class Navigation:
+    """ Class to control position based on GPS input 
+    
+    Attributes:
+        GPS - GPS Interface object, used to obtain GPS coordinates
+        steering - Steering object
+        current_position - The current position of the TBM in 3-D space (x, y, z)
+        desired_position - The desired position of the TBM in 3-D space (x, y, z)
+    
+    Methods:
+        set_desired_position() 
+        update_current_position()  
+        navigate()
+    """
+    
     def __init__(self, GPS, steering):
 
         # Instances of the two classes defined below for use to complete the navigation method
@@ -45,95 +63,118 @@ class Navigation:
         self.current_position = None
         self.desired_position = None
     
-    """ Sets the desired position that the borer will attempt to move to
-    
-    Note: assume the user will pass in the desired_position parameter when using
-          the interface
-    """
+
     def set_desired_position(self, desired_position):
+        """ Sets the desired position the TBM will attempt to move to.
+        
+        Note: assume the user will pass in the desired_position parameter when using
+        the interface 
+        """
         pass
 
-    """ Updates the current position """
-    def update_current_position(self):
-        pass
-
-    """ Navigate to the desired position from the current position
     
-    Based on the current position tuple, compared to the desired position tuple,
-    make decisions on steering, and ensure that the actuations are successful
-
-    Returns: True if actuation requests were successful, False if not
-    Note: It may be good to notify the user if something unexpected happens!
-    """
-    def navigate(self):
+    def update_current_position(self):
+        """ Updates the current position of the TBM """
         pass
 
 
-# Code below is provided for you, DO NOT NEED TO IMPLEMENT
+    def navigate(self):
+        """ Navigate to the desired position from the current position
+        
+        Based on the current position tuple, compared to the desired position tuple,
+        make decisions on steering, and ensure that the actuations are successful
 
-""" Interfaces with the GPS Sensor """
+        Returns: True if actuation requests were successful, False if not
+        Note: It may be good to notify the user if something unexpected happens!
+        """
+        pass
+
+
+# Code below is provided for you, YOU DO NOT NEED TO IMPLEMENT THIS
+   
 class GPS:
+    """ Mock GPS sensor interface class """
+    
     def __init__(self, GPSSensor):
         self.GPS = GPSSensor
         self.x = GPSSensor.pos.x
         self.y = GPSSensor.pos.y
         self.z = GPSSensor.pos.z
 
-    """ Polls the GPS for the current position
-
-    Modifies the leftOf, rightOf, and tunnelPos fields to contain the current
-    position
-    """
     def pollSensor(self):
-        self.x, self.y, self.z = self.GPS.updatePosition()
+        """ Polls the GPS for the current position
+
+        Updates the current position by reading the gps sensor
+        """
+        self.x, self.y, self.z = self.GPS.read()
     
-    """ Returns the positions stored in the leftOf, rightOf, and tunnelPos fields as a tuple """
+   
     def getPos(self):
+        """ Returns the TBM position """
         return self.x, self.y, self.z
 
-""" Interfaces with the actuator which steers the tunnel borer, and controls its speed """
+
 class Steering:
+    """ Interfaces with the actuator which steers the TBM, and controls its speed 
+    
+    Attributes:
+        act - Placeholder actuation component (could be a motor for example, or some other actuator related to steering)
+
+    Methods:
+        move_left(left_distance)
+        move_right(right_distance)
+        move_down(down_distance)
+        move_up(up_distance)
+        move_forward()
+        stop()
+    """
+    
     def __init__(self, actuation_component):
         self.act = actuation_component
 
-    """ Steers the borer to the left
 
-    Returns: True if the actuation is successful, False if it is not
-    """
     def move_left(self, left_distance):
+        """ Steers the TBM left
+
+        Returns: True if the actuation is successful, False if it is not
+        """
         return self.act.steer_left(left_distance)
         
-    """ Steers the borer to the right
-
-    Returns: True if the actuation is successful, False if it is not
-    """
     def move_right(self, right_distance):
+        """ Steers the TBM to the right
+
+        Returns: True if the actuation is successful, False if it is not
+        """
         return self.act.steer_right(right_distance)
 
-    """ Steers the borer down
+    def move_down(self, down_distance): 
+        """ Steers the TBM down
 
-    Returns: True if the actuation is successful, False if it is not
-    """
-    def move_down(self, down_distance):
+        Returns: True if the actuation is successful, False if it is not
+        """
         return self.act.steer_down(down_distance)
 
-    """ Steers the borer to the right
 
-    Returns: True if the actuation is successful, False if it is not
-    """
     def move_up(self, up_distance):
+        """ Steers the TBM to the right
+
+        Returns: True if the actuation is successful, False if it is not
+        """
         return self.act.steer_up(up_distance)
 
-    """ Moves the borer forward
 
-    Returns: True if the motor sucessfully moves the borer, False if it is not
-    """
     def move_forward(self):
+        """ Moves the TBM forward
+
+        Returns: True if the motor sucessfully moves the TBM, False if it is not
+        """
         return self.act.forward()
 
-    """ Stops the borer from moving
 
-    Returns: True if the motor stops the borer, False if it does not
-    """
     def stop(self):
+        """ Stops the TBM from moving
+
+        Returns: True if the motor stops the TBM, False if it does not
+        """
         return self.act.stop()
+    
