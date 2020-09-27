1a6a117d90993ff2a8ce55e21ac3a34afc749724
Shamima Ali <snali@mun.ca>
2020-09-27 01:04:42 -0230

    Added code for navigate

--- fall-2020/navigation.py
+++ fall-2020/navigation.py
@@ -93,6 +93,38 @@ class Navigation:
         Returns: True if actuation requests were successful, False if not
         Note: It may be good to notify the user if something unexpected happens!
         """
+        act_request_forward = True
+        act_request_right = True
+        act_request_left = True
+        act_request_up = True
+        act_request_down = True
+
+        if desired_position[0] > current_position[0]:
+            while(current_position[0] != desired_position[0]):
+                act_request_forward = Steering.move_forward()
+                self.update_current_position
+                if act_request_forward == False:
+                    print("Unable to move to desired x position")
+        
+        required_y_distance = desired_position[1] - current_position[1]
+        if required_y_distance > 0:
+            act_request_right = Steering.move_right(required_y_distance)
+        elif required_y_distance < 0:
+            act_request_left = Steering.move_left(required_y_distance)
+        if act_request_left or act_request_left == False:
+            print("Unable to move to desired y position")
+            
+        required_z_distance = desired_position[2] - current_position[2]
+        if required_z_distance > 0:
+            act_request_up = Steering.move_up(required_z_distance)
+        elif required_z_distance < 0:
+            act_request_down = Steering.move_down(required_z_distance)        
+        if act_request_up or act_request_down == False:
+            print("Unable to move to desired z position")       
+
+        self.update_current_position
+        return current_position == desired_position
+        
         pass
 
 
